295d840d14dd314af5662194be99528034040c95
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2025-05-16 23:45:53 +0600

    add cubit

--- lib/cubit/surah_cubit.dart
+++ lib/cubit/surah_cubit.dart
@@ -1,8 +1,22 @@
+import 'package:al_quran_app/models/surah_list_model.dart';
+import 'package:al_quran_app/services/api_service.dart';
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 part 'surah_state.dart';
 
 class SurahCubit extends Cubit<SurahState> {
   SurahCubit() : super(SurahInitial());
+
+  void fetchSurahs() async {
+    try {
+      emit(SurahLoading());
+      final surahs = await ApiService.fetchSurahs();
+      emit(SurahLoaded(surahs));
+    } catch(e) {
+      emit(SurahError(e.toString()));
+    }
+  }
 }
--- lib/cubit/surah_state.dart
+++ lib/cubit/surah_state.dart
@@ -1,6 +1,28 @@
 part of 'surah_cubit.dart';
 
-@immutable
-sealed class SurahState {}
+abstract class SurahState extends Equatable{
+  List<Object?> get props => [];
+}
 
 final class SurahInitial extends SurahState {}
+
+class SurahLoading extends SurahState {}
+
+class SurahLoaded extends SurahState {
+  final List<Data> surahs;
+  SurahLoaded(this.surahs);
+
+  @override
+  List<Object?> get props => [surahs];
+}
+
+class SurahError extends SurahState {
+  final String message;
+  SurahError(this.message);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [message];
+}
+
+
